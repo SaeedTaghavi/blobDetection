34973d805d17e74b0795539442bad8e64ab1339a
saeedtaghavi <saeed.taghavi.v@gmail.com>
2021-11-17 17:25:03 +0330

    first release

--- frames/main.py
+++ frames/main.py
@@ -1,15 +1,11 @@
 import cv2
-frames = []
-# vidcap = cv2.VideoCapture('2021-10-21_15-26-51.mp4')
-filename ='test1_Y-Z.mp4'
-vidcap = cv2.VideoCapture('test1_Y-Z.mp4')
+filename ='test1_X-Z.mp4'
+vidcap = cv2.VideoCapture(filename)
 success,image = vidcap.read()
-frames.append(image)
 count = 0
 while success:
-  # cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
+  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
   success,image = vidcap.read()
   print('Read a new frame: %d ' % count, success)
-  frames.append(image)
   count += 1
 print ("num frames: ", len(frames))
